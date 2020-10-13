b1638db0c6b8c1891876fe4faedc061de6557e7c
Alexlionman <pereirasander33@gmail.com>
2020-10-13 13:45:04 -0300

    Inicializado.

---
+++ src/main/java/model/Calcular.java
@@ -0,0 +1,125 @@
+
+package model;
+
+import java.util.Random;
+
+/**
+ *
+ * @author a.pereira
+ */
+public class Calcular {
+    private int dificuldade;
+    private int valor1;
+    private int valor2;
+    private int operacao;
+    private int resultado;
+
+    public Calcular(int dificuldade) {
+        
+        Random rand = new Random();
+        
+        this.operacao = rand.nextInt(3); // 0 = somar, 1 =  diminuir, 2 = multiplicar
+        this.dificuldade = dificuldade;
+        
+        if(dificuldade == 1){
+          //1 = facil
+          this.valor1 = rand.nextInt(10); //gera um valor entre 0 e 9
+          this.valor2 = rand.nextInt(10); 
+        }else if(dificuldade == 2 ){
+          //2 = medio
+          this.valor1 = rand.nextInt(100); //gera um valor entre 0 e 99
+          this.valor2 = rand.nextInt(100); 
+        }else if(dificuldade == 3){
+           //3 = dificil
+          this.valor1 = rand.nextInt(1000); //gera um valor entre 0 e 999
+          this.valor2 = rand.nextInt(1000); 
+        }else if(dificuldade == 4 ){
+           //4 = muito dificil
+          this.valor1 = rand.nextInt(10000); //gera um valor entre 0 e 9999
+          this.valor2 = rand.nextInt(10000); 
+        }else{
+           //ultra
+          this.valor1 = rand.nextInt(100000); //gera um valor entre 0 e 99999
+          this.valor2 = rand.nextInt(100000); 
+        }
+    }
+    
+    
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public int getValor1() {
+        return valor1;
+    }
+
+    public int getValor2() {
+        return valor2;
+    }
+    
+    public int getOperacao() {
+        return operacao;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+    
+    public String toString(){
+        String op;
+        if(this.getOperacao() == 0){
+        op = "Somar";
+        }else if(this.getOperacao() == 1){
+        op = "Subtrair";
+        }else if(this.getOperacao() == 2){
+        op = "Multiplicar";
+        }else{
+        op = "Operação desconhecida";
+        }
+        return "Valor 1: " + this.getValor1() +
+                "\nValor 2: " + this.getValor2() +
+                "\nDificuldade: " + this.getDificuldade() +
+                "\nOperação: " + op;
+        
+    }
+        
+    public boolean somar(int resposta){
+    this.resultado = this.getValor1() + this.getValor2();
+    boolean certo = false;
+    if(resposta == this.getResultado()){
+        System.out.println("Resposta correta");
+        certo =  true;
+    }else{
+        System.out.println("Respota errada");
+    }
+        System.out.println(this.getValor1() + " + " + this.getValor2() + " = " + this.getResultado());
+        return certo;
+    }
+    
+    public boolean subtrair(int resposta){
+    this.resultado = this.getValor1() - this.getValor2();
+    boolean certo = false;
+    if(resposta == this.getResultado()){
+        System.out.println("Resposta correta");
+        certo =  true;
+    }else{
+        System.out.println("Respota errada");
+    }
+        System.out.println(this.getValor1() + " - " + this.getValor2() + " = " + this.getResultado());
+        return certo;
+    }
+    
+    public boolean multiplicar(int resposta){
+    this.resultado = this.getValor1() * this.getValor2();
+    boolean certo = false;
+    if(resposta == this.getResultado()){
+        System.out.println("Resposta correta");
+        certo =  true;
+    }else{
+        System.out.println("Respota errada");
+    }
+        System.out.println(this.getValor1() + " X " + this.getValor2() + " = " + this.getResultado());
+        return certo;
+    }
+
+}
